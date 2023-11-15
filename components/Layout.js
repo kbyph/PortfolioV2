dfa6f8b8bc038f9bf288d8f7c96e6f7e4baef4af
Kobe Pho <kobeypho@gmail.com>
2023-11-15 15:24:54 -0800

    Corrected Typo

--- components/Layout.js
+++ components/Layout.js
@@ -11,12 +11,10 @@ const sora = Sora({
 //Components
 import Nav from '../components/Nav'
 import Header from '../components/Header'
-import TopLeftImg from '../components/TopLeftImg'
 
 const Layout = ({ children }) => {
   return (
   <div className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}>
-    <TopLeftImg />
     <Nav />
     <Header />
     {children}
